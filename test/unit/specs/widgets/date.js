d0fcbd2531e96ce179ebe820bafcc25c892b4b30
MacGyverOfCode <richard_a_roth@yahoo.com>
2016-12-06 11:28:15 +0000

    Today and Tomorrow now set correct dates

---
+++ test/unit/specs/widgets/date.js
@@ -0,0 +1,48 @@
+/*
+* Widget Tests
+*/
+
+'use strict'
+
+import {expect} from 'chai'
+import jsdom from 'mocha-jsdom'
+import {Widget} from '../../../../src/widgets/core'
+import {StringField} from '../../../../src/fields/date'
+
+describe('Widget', function() {
+    var TestField, TestWidget
+
+    beforeEach('create an TestWidget instance', function() {
+        jsdom()
+
+        TestField = new StringField({
+            id: 'test-string', 
+            name: 'TestString', 
+            label: 'Test String Field'
+        })
+
+        TestWidget = new DateSelectionInput(
+            TestField, 
+            'string', 
+            'test-widget', 
+            'TestWidget', 
+            'Test Widget'
+        )
+    })
+
+    describe('#setDateValue()', function() {
+        it('sets a valid date value', function() {
+            let date = new Date()
+            TestWidget.setDateValue(date)
+            expect(TestWidget.value).to.equal(date)
+        })
+    })
+
+    /* describe('#getDateValue()', function() {
+        it('return a valid date value', function() {
+            let testValue = 'Test'
+            TestWidget.value = testValue
+            expect(TestWidget.getValue()).to.equal(testValue)
+        })
+    }) */
+})
