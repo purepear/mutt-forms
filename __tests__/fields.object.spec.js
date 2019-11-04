8b19d889730cde86a22451d299024e1dbb878a4e
Craig Blaszczyk <craig@boughtbymany.com>
2019-11-04 15:43:01 +0000

    Allow object fields to run their own validators
    This allows us to run validators which validate on a multi-field level,
    e.g. "if field1 has a value then field2 must have one too"

--- __tests__/fields.object.spec.js
+++ __tests__/fields.object.spec.js
@@ -1,6 +1,7 @@
 "use strict"
 
 import Mutt from "../src/index"
+import { Validator } from "../src/validators"
 
 describe("Object Field", () => {
   test("test objects serialize correctly", () => {
@@ -74,5 +75,95 @@ describe("Object Field", () => {
     expect(nameField.value).toEqual("example")
     expect(otherField.value).toEqual("test")
     expect(telField.value).toEqual("01234567890")
+  })
+
+  describe("validator", () => {
+    const schema = {
+      type: "object",
+      properties: {
+        colour: {
+          type: "string"
+        },
+        name: {
+          type: "string"
+        },
+        email: {
+          type: "string"
+        }
+      }
+    }
+    class NoMuppetsValidator extends Validator {
+      validate(value) {
+        if (value.name == "Kermit" || value.email.endsWith("muppets.com")) {
+          this.error = {
+            name: "Muppets are not allowed",
+            email: "Muppets are not allowed"
+          }
+          return false
+        }
+
+        return true
+      }
+    }
+    class NoTestEmailsValidator extends Validator {
+      validate(value) {
+        if (value === "test@example.com") {
+          this.error = "Only real email addresses are allowed"
+          return false
+        }
+        return true
+      }
+    }
+
+    const options = {
+      email: {
+        validators: [new NoTestEmailsValidator()]
+      },
+      validators: [new NoMuppetsValidator()]
+    }
+
+    const field = Mutt.fields.ObjectField.new("test", "test", schema, options)
+
+    test("allows valid values", () => {
+      field.value = {
+        colour: "green",
+        name: "Wonder Woman",
+        email: "wonderwoman@example.com"
+      }
+
+      expect(field.validate()).toBe(true)
+    })
+
+    test("errors on invalid values", () => {
+      field.value = {
+        colour: "green",
+        name: "Kermit",
+        email: "test@example.com"
+      }
+      field.object.name.widget.refreshErrorState = jest.fn()
+      field.object.email.widget.refreshErrorState = jest.fn()
+
+      expect(field.validate()).toBe(false)
+      expect(field.errors).toEqual({
+        name: ["Muppets are not allowed"],
+        email: [
+          "Only real email addresses are allowed",
+          "Muppets are not allowed"
+        ]
+      })
+      expect(field.object.colour.errors).toEqual([])
+      expect(field.object.email.errors).toEqual([
+        "Only real email addresses are allowed",
+        "Muppets are not allowed"
+      ])
+      expect(field.object.email.widget.refreshErrorState).toHaveBeenCalledWith([
+        "Only real email addresses are allowed",
+        "Muppets are not allowed"
+      ])
+      expect(field.object.name.errors).toEqual(["Muppets are not allowed"])
+      expect(field.object.name.widget.refreshErrorState).toHaveBeenCalledWith([
+        "Muppets are not allowed"
+      ])
+    })
   })
 })
